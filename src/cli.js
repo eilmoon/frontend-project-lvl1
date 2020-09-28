11b17e26189bff0f5b5879c77bed71c797b756f7
eilmoon <eilmoon@yandex.ru>
2020-09-28 17:53:36 +0300

    step 3 - done

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import promptly from 'promptly';
+
+export default async () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = await promptly.prompt('May I have your name?: ');
+    console.log(`Hello, ${userName}!`);
+};
