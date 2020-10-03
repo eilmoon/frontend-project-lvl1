ff4392b55db481bd9d09878bfaaab4881ae1548e
eilmoon <eilmoon@yandex.ru>
2020-10-03 13:45:10 +0300

    even game multy games based cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,22 @@
 import promptly from 'promptly';
 
-export default async () => {
+//startGame
+export default async (description, gameData) => {
   console.log('Welcome to the Brain Games!');
   const userName = await promptly.prompt('May I have your name?: ');
   console.log(`Hello, ${userName}!`);
+
+  console.log(description);//описание игры
+  for (let i = 0; i < 3; i += 1) { //играем до трёх побед
+    const [question, correctAnswer] = gameData();
+    console.log(`Question: ${question}`);
+    const answer = await promptly.prompt('Your answer: ');
+    if (answer !== correctAnswer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
