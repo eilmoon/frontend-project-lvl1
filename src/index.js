e4227290a8251a452939daa275de4211b9411232
eilmoon <eilmoon@yandex.ru>
2020-10-08 11:01:39 +0300

    almost linted!

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import promptly from 'promptly';
 
-// startGame
-
 export default async (description, gameData) => {
   console.log('Welcome to the Brain Games!');
   const userName = await promptly.prompt('May I have your name?: ');
   console.log(`Hello, ${userName}!`);
-
-  console.log(description); // описание игры
-  for (let i = 0; i < 3; i += 1) { // играем до трёх побед
+  const numberOfQuestions = 3;
+  console.log(description);
+  for (let i = 0; i < numberOfQuestions; i += 1) {
     const [question, correctAnswer] = gameData();
     console.log(`Question: ${question}`);
     const answer = await promptly.prompt('Your answer: ');
@@ -21,3 +19,12 @@ export default async (description, gameData) => {
   }
   console.log(`Congratulations, ${userName}!`);
 };
+
+// " Библиотека promptly работает в асинхронном режиме который мы еще не изучали
+// Для ее работы нужно выполнить две вещи:
+// 1. Добавить слово async перед определением функции
+// 2. Добавить слово await перед каждым вызовом метода prompt
+// В остальном код совершенно обычный "
+
+/* p.s. Пошёл изучать https://ru.hexlet.io/courses/js-asynchronous-programming , разберусь с промисами - вернусь
+*/
